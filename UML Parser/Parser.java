c3b4ae94abddaf7c31f7efdd7ac31553f7adc080
DixitaPatodiya <patodiyadixita@gmail.com>
2017-04-06 01:02:57 -0700

    Update Parser.java

--- UML Parser/Parser.java
+++ UML Parser/Parser.java
@@ -7,3 +7,30 @@ import com.github.javaparser.ast.comments.*;
 import com.github.javaparser.ast.expr.*;
 import com.github.javaparser.ast.stmt.*;
 import com.github.javaparser.ast.type.*;
+
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.ModifierSet;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.body.VariableDeclaratorId;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.PrimitiveType;
+import com.github.javaparser.ast.type.ReferenceType;
+import com.github.javaparser.ast.type.VoidType;
+
+public class Java2UMLParser {
+private static String filePath = null;// "D:/projects/UMLJavaParser/src/javaparser";
+	private static File folder = null;//new File(filePath+"/"+"testcase5");
+	private static File[] listOfFiles = null;//folder.listFiles();
+	private StringBuilder bodyURL = new StringBuilder();
+
+}
