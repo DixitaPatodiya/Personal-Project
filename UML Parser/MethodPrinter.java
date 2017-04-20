ea939b734c3682af9f3e2254032233f596385a76
DixitaPatodiya <patodiyadixita@gmail.com>
2017-04-20 12:51:48 -0700

    Create MethodPrinter.java

---
+++ UML Parser/MethodPrinter.java
@@ -0,0 +1,34 @@
+import java.io.FileInputStream;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+public class MethodPrinter {
+
+    public static void main(String[] args) throws Exception {
+        // creates an input stream for the file to be parsed
+        FileInputStream in = new FileInputStream("D:\\Study\\USAdoc\\SJSU\\Subjects\\202\\workspace\\umlparser1\\uml-parser-test-4\\Observer.java");
+
+        // parse it
+        CompilationUnit cu = JavaParser.parse(in);
+
+        // visit and print the methods names
+        new MethodVisitor().visit(cu, null);
+    }
+
+    /**
+     * Simple visitor implementation for visiting MethodDeclaration nodes.
+     */
+    private static class MethodVisitor extends VoidVisitorAdapter<Void> {
+        @Override
+        public void visit(MethodDeclaration n, Void arg) {
+            /* here you can access the attributes of the method.
+             this method will be called for all methods in this 
+             CompilationUnit, including inner class methods */
+            System.out.println(n.getName());
+            super.visit(n, arg);
+        }
+    }
+}
